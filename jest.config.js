79371ae8bc23d511bf6c5f319299ba1b2f81a188
Caro-L-dev <liardcar@gmail.com>
2024-04-05 17:38:14 +0200

    chore: setup jest & react testing library

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+export default {
+  testEnvironment: "jsdom",
+
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.(ts|tsx)?$": [
+      "ts-jest",
+      { diagnostics: { ignoreCodes: ["TS151001"] } },
+    ],
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.svg$": "jest-transformer-svg",
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
